ff140da1f5d83914665a78d2027fd8aeda402826
pegasus1982 <gdm19920108@gmail.com>
2019-02-12 04:32:02 +0800

    load model completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,44 @@ var canvas = document.getElementById('renderCanvas');
 // load the 3D engine
 var engine = new BABYLON.Engine(canvas, true);
 
-var scene,camera,light,sphere,ground;
+var scene,
+    camera,
+    light1,
+    light2,
+    light3,
+    sphere,
+    ground;
 // createScene function that creates and return the scene
 var createScene = function(){
     scene = new BABYLON.Scene(engine);
     
-    camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(8, 5,-10), scene);
-    camera.setTarget(BABYLON.Vector3.Zero());
+    camera = new BABYLON.ArcRotateCamera("Camera", 4, 1, 300, new BABYLON.Vector3(0,30,0), scene);
+    camera.attachControl(canvas, false);
 
-    // attach the camera to the canvas
-    // camera.attachControl(canvas, false);
+    light1 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(200, 200, 100), scene);
+    light2 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(-400, 500, -400), scene);
 
-    light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
+    // sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
+    // sphere.position.y = 1;
 
-    sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
-    sphere.position.y = 1;
+    // ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
 
-    ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
+    //load babylon model
+    var assetsManager = new BABYLON.AssetsManager(scene);
+    var meshTask = assetsManager.addMeshTask("model task", "", "assets/models/babylon/", "model.babylon");
 
+    meshTask.onSuccess = function (task) {
+        // task.loadedMeshes[0].position = new BABYLON.Vector3(0, 0, 0);
+        // engine.loadingUIText = "Loaded asset " + task.loadedMeshes[0].name;
+        console.log("load task successed");
+        console.log(task.loadedMeshes)
+    }
+
+    meshTask.onError = function(task,message,exception){
+        console.log(task,message,exception);
+    }
+
+    assetsManager.load();
     return scene;
 }
 
